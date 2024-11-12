a74140ceb3abcd7978c9d5407289a789e3eca3f8
Insanerst <aelwa001@hotmail.com>
2024-11-12 13:42:16 +0200

    added files needed for imports and added login UI to ui.py

--- Logic/login.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def validate_password(password):
-    pattern = r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$'
-    if re.match(pattern, password):
-        return True
-    else:
-        return False
